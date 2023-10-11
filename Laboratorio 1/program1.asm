cec4294c3123cfdeb152b780bf52602c45ec5618
Agustin Vera <agustin.vera@usach.cl>
2023-10-10 23:32:59 -0300

    Se agrega la Actividad 1 del Laboratorio 1 - Escribir Programas MIPS

---
+++ Laboratorio 1/program1.asm
@@ -0,0 +1,11 @@
+addi $s0, $0, 0	     # a = 0
+addi $s1, $0, 1      # z = 1
+addi $s2, $0, 10    # Numero que sera condicion en el while
+While:
+	beq $s2, $s1, EndWhile    # Si $s2 = $s1 salta a EndWhile
+	add $s0, $s0, $s1      # a = a + z
+	addi $s1, $s1, 1       # z = z + 1
+	j While                # Salta a While
+EndWhile:
+	li $v0, 10 # Termino del programa, exit, instruccion con un 10
+	syscall
